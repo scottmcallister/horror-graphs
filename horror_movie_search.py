132cdf9959d66545c2c9caf5707c01325b35a755
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-03-13 22:33:26 -0400

    make query for movies dynamic

--- horror_movie_search.py
+++ horror_movie_search.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_restful import reqparse, Resource, Api
 import json
 import psycopg2
 import psycopg2.extras
-from flask import jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +15,16 @@ host = "127.0.0.1"
 port = 5432
 
 
+def gen_movie_query(args):
+    query = 'select * from movies'
+    for arg in args:
+        query += {
+            arg == 'keywords': ' where title like "%"'
+        }
+    query += ';'
+    return query
+
+
 @app.route('/')
 def index():
     return render_template('index.html', title="Horror Movie Search")
@@ -23,14 +32,47 @@ def index():
 
 @app.route('/api/movie')
 def movies():
+    keywords = request.args.get('keywords') or ''
+    director = request.args.get('director') or ''
+    year_min = request.args.get('year_min') or 0
+    year_max = request.args.get('year_max') or 3000
+    critic_min = request.args.get('critic_min') or 0
+    critic_max = request.args.get('critic_max') or 101
+    user_min = request.args.get('user_min') or 0
+    user_max = request.args.get('user_max') or 101
+    country = request.args.get('country') or ''
+    page = request.args.get('page') or 1
+    offset = (page - 1) * 20
     con = psycopg2.connect(database=dbname, user=user, password=password,
                            host=host, port=port)
     cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute('select * from movie limit 20;')
+    cur.execute('SELECT * FROM movie \
+                WHERE title ILIKE %s \
+                AND director ILIKE %s \
+                AND country ILIKE %s \
+                AND year >= %s \
+                AND year <= %s \
+                AND critic_score >= %s \
+                AND critic_score <= %s \
+                AND user_score >= %s \
+                AND user_score <= %s \
+                ORDER BY critic_score desc \
+                limit 20 offset %s',
+                ('%' + keywords + '%',
+                 '%' + director + '%',
+                 '%' + country + '%',
+                 year_min,
+                 year_max,
+                 critic_min,
+                 critic_max,
+                 user_min,
+                 user_max,
+                 offset))
+    print(cur.query)
     movies = cur.fetchall()
-    print(movies)
     cur.connection.close()
     return jsonify({'movies': movies})
+    return jsonify({'args': request.args})
 
 
 if __name__ == '__main__':
