32adc9776e2329698b397b045665f90f0709353f
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-03-15 19:14:44 -0400

    fix linting errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
+/* eslint no-undef: 0 */
 var path = require('path');
-var webpack = require('webpack');
 
 var config = {
-  context: __dirname
+    context: __dirname
 };
 
 config.entry = [
-  'babel-polyfill',
-  path.join(__dirname, 'src', 'js', 'main.js')
+    'babel-polyfill',
+    path.join(__dirname, 'src', 'js', 'main.js')
 ];
 
 config.module = {
-  loaders: [
-    {
-      loader:  'babel-loader',
-      test:    /\.jsx?$/,
-      exclude: /node_modules/
-    }
-  ],
-  postLoaders: []
+    loaders: [
+        {
+            loader:  'babel-loader',
+            test:    /\.jsx?$/,
+            exclude: /node_modules/
+        }
+    ],
+    postLoaders: []
 };
 
 config.output = {
-  filename: '[name].bundle.js',
-  chunkFilename: '[id].chunk.js',
-  path: path.join(__dirname, 'static', 'js'),
-  publicPath: '/static/js',
-  devtoolModuleFilenameTemplate: '[resourcePath]',
-  devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js',
+    path: path.join(__dirname, 'static', 'js'),
+    publicPath: '/static/js',
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
 };
 
 config.plugins = [];
 
 config.resolve = {
-  extensions: [
-    '',
-    '.js',
-    '.jsx'
-  ],
-  modulesDirectories: [
-    './src/js',
-    './node_modules/'
-  ]
+    extensions: [
+        '',
+        '.js',
+        '.jsx'
+    ],
+    modulesDirectories: [
+        './src/js',
+        './node_modules/'
+    ]
 };
 
 module.exports = config;
