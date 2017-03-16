bd09aace129a1f10b9273566858516e811df0409
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-03-16 13:06:01 -0400

    add images to search results

--- src/js/scenes/SearchPage/components/SearchResults/components/MovieListing/index.jsx
+++ src/js/scenes/SearchPage/components/SearchResults/components/MovieListing/index.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const MovieListing = (props) => {
-    return (<h1>{props.title}</h1>);
+const getPosterUrl = (name, year) => {
+    const fixed_name = name.replace(/([^\s\w]|_)+/g, '')
+        .replace(/\s/g, '_')
+        .toLowerCase();
+    const url = `/static/images/${fixed_name}${year}.png`;
+    return url;
 };
+
+class MovieListing extends Component {
+    static propTypes = {
+        country: PropTypes.string.isRequired,
+        critic_score: PropTypes.number.isRequired,
+        director: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired,
+        rt_url: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        user_score: PropTypes.number.isRequired,
+        year: PropTypes.number.isRequired,
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false,
+        };
+    }
+
+    render() {
+        const { hasError } = this.state;
+        return (
+            <div className="col-md-4 col-sm-6">
+                <div className="thumbnail">
+                    <img
+                        src={hasError ? '/static/images/image-404.png' : getPosterUrl(this.props.title, this.props.year)}
+                        alt={this.props.title}
+                        onError={() => this.setState({ hasError: true })}
+                    />
+                    <div className="caption">
+                        <h3>{this.props.title}</h3>
+                    </div>
+                </div>
+            </div>);
+    }
+}
 
 export default MovieListing;
